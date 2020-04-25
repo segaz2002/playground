c9e6e14e9ad57d01436ae15c063efaa9cc874d4f
Gabriel <segaz2002@gmail.com>
2020-04-25 15:14:22 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// write hello world program
+// print out a greeting with a name e.g Hello John Doe
+// how to print to console in javascript
+
+//Datatypes
+
+// String example, "Movefdfgdgfdfgdffgdf"
+// Numbers e.g float or integer e.g 12.02, 12
+// Arrays e.g list of product names ["product1", "product2"],
+// Object e.g {"name": "some name","department": "some", "type", id: 12345}
+
+//Arithmetc operators
+// +, -, %, /, >=, <=, ==
+
+//Logical operators
+// and &&
+// or ||
+// not ! 
+
+
+// why do we need git (github)
+// Git versions 
+// => (it is keeping track of our revisions)
+// => (it is keeping track of our revisions)
+// =>
+
+
+//Begin
+//accept a name
+var name = "John Doe";
+// prepare greeting
+var greeting = "Hello " + name;
+// print out greeting
+console.log(greeting);
+// end
+
+
