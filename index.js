81ca0ba968818a883044d04cfd2095e46f6b7256
Gabriel <segaz2002@gmail.com>
2020-04-29 19:46:24 +0300

    git clone

--- index.js
+++ index.js
@@ -5,9 +5,27 @@
 //Datatypes
 
 // String example, "Movefdfgdgfdfgdffgdf"  or 'somestring'
+var name = "Femi";
+
 // Numbers e.g float or integer e.g 12.02, 12
+var x = 2;
+
 // Arrays e.g list of product names ["product1", "product2"],
+var numbers = [2, 4, 5, 6];
+var names = ["name1", "name2"];
+
 // Object e.g {"name": "some name","department": "some", "type", id: 12345}
+var girl = { name: "Aliona", "sex": "Female" };
+var car = { make: "Audi", chasisNumber: 12121212, color: "Red" };
+var student = { id: 343434, year: 1, course: "Computer Science" };
+
+// Function
+var identificationNumber = function (person) {
+  return person.id;
+}
+identificationNumber({ id: 1212 });
+// A variablle as address to some data in memory 
+
 
 //Arithmetc operators
 // +, -, %, /, >=, <=, ==, ===
@@ -33,6 +51,8 @@
 // git commit -m "<some message>" # add new changes to the repo
 // git diff # shows you the difference the in change and the last tracked point
 // git push #transfers the local changes to the remote repo
+// git clone <repository url>
+
 
 
 //Begin
